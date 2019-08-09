b4d6f4c53424b862a88e3c94eec3dc7b7d5e9932
shtian@isoftftone.com <Tsh85641399>
2019-08-09 12:23:48 +0800

    修改ui设置

--- src/sample/UICreator.java
+++ src/sample/UICreator.java
@@ -5,8 +5,8 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import utils.StringUtils;
 
 public class UICreator {
@@ -21,14 +21,35 @@ public class UICreator {
     static long lastResult;
     static String lastOperator;
     static boolean clearLabel = false;
+    static double buttonWidth = 40;
+    static double buttonHeight = 20;
 
     public static Label displayScreen(){
+        output.setTextAlignment(TextAlignment.RIGHT);
         output.setFont(Font.font(30));
         return output;
     }
 
-    public static VBox displaySign(){
-        VBox vBox = new VBox(2);
+    public static GridPane displayCenter(){
+
+        // 添加数字键
+        GridPane centerSection = new GridPane();
+        centerSection.setPadding(new Insets(5,5,5,5));
+        centerSection.setVgap(10);
+        centerSection.setHgap(10);
+
+        Button btnNumDot = new Button(".");
+        Button btnNum0 = new Button("0");
+        Button btnNum1 = new Button("1");
+        Button btnNum2 = new Button("2");
+        Button btnNum3 = new Button("3");
+        Button btnNum4 = new Button("4");
+        Button btnNum5 = new Button("5");
+        Button btnNum6 = new Button("6");
+        Button btnNum7 = new Button("7");
+        Button btnNum8 = new Button("8");
+        Button btnNum9 = new Button("9");
+
         Button btnClear = new Button("CE");
         Button btnBack = new Button("<-");
         Button btnTimes = new Button("X");
@@ -36,6 +57,76 @@ public class UICreator {
         Button btnPlus = new Button("+");
         Button btnMinus = new Button("-");
         Button btnEquals = new Button("=");
+        Button btnEmpty = new Button();
+
+
+        btnClear.setPrefSize(buttonWidth,buttonHeight);
+        btnBack.setPrefSize(buttonWidth,buttonHeight);
+        btnEmpty.setPrefSize(buttonWidth,buttonHeight);
+        btnDivide.setPrefSize(buttonWidth,buttonHeight);
+        btnTimes.setPrefSize(buttonWidth,buttonHeight);
+        btnMinus.setPrefSize(buttonWidth,buttonHeight);
+        btnPlus.setPrefSize(buttonWidth,buttonHeight);
+        btnEquals.setPrefSize(buttonWidth,buttonHeight);
+        btnNumDot.setPrefSize(buttonWidth,buttonHeight);
+        btnNum0.setPrefSize(buttonWidth,buttonHeight);
+        btnNum1.setPrefSize(buttonWidth,buttonHeight);
+        btnNum2.setPrefSize(buttonWidth,buttonHeight);
+        btnNum3.setPrefSize(buttonWidth,buttonHeight);
+        btnNum4.setPrefSize(buttonWidth,buttonHeight);
+        btnNum5.setPrefSize(buttonWidth,buttonHeight);
+        btnNum6.setPrefSize(buttonWidth,buttonHeight);
+        btnNum7.setPrefSize(buttonWidth,buttonHeight);
+        btnNum8.setPrefSize(buttonWidth,buttonHeight);
+        btnNum9.setPrefSize(buttonWidth,buttonHeight);
+
+
+
+
+        // 布局横坐标
+        int i = 0;
+        // 布局纵坐标
+        int j = 0;
+        logger.info(String.format("column = %d, row = %d", i, j));
+
+        GridPane.setConstraints(btnClear, i, j);
+        GridPane.setConstraints(btnEmpty, ++i,j);
+        GridPane.setConstraints(btnBack, ++i,j);
+        GridPane.setConstraints(btnDivide, ++i,j);
+        // 换行
+        i = 0;
+        j++;
+        logger.info(String.format("column = %d, row = %d", i, j));
+        GridPane.setConstraints(btnNum7, i,j);
+        GridPane.setConstraints(btnNum8, ++i,j);
+        GridPane.setConstraints(btnNum9, ++i,j);
+        GridPane.setConstraints(btnTimes, ++i,j);
+        // 换行
+        i = 0;
+        j++;
+        logger.info(String.format("column = %d, row = %d", i, j));
+        GridPane.setConstraints(btnNum4, i,j);
+        GridPane.setConstraints(btnNum5, ++i,j);
+        GridPane.setConstraints(btnNum6, ++i,j);
+        GridPane.setConstraints(btnMinus, ++i,j);
+        // 换行
+        i = 0;
+        j++;
+        logger.info(String.format("column = %d, row = %d", i, j));
+        GridPane.setConstraints(btnNum1, i,j);
+        GridPane.setConstraints(btnNum2, ++i,j);
+        GridPane.setConstraints(btnNum3, ++i,j);
+        GridPane.setConstraints(btnPlus, ++i,j);
+        // 换行
+        i = 1;
+        j++;
+        logger.info(String.format("column = %d, row = %d", i, j));
+        GridPane.setConstraints(btnNum0, i,j);
+        GridPane.setConstraints(btnNumDot, ++i,j);
+        GridPane.setConstraints(btnEquals, ++i,j);
+
+        centerSection.getChildren().addAll(btnClear, btnEmpty, btnBack, btnDivide, btnEquals, btnPlus, btnMinus, btnTimes,
+                btnNum1, btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9, btnNum0, btnNumDot);
 
         btnBack.setOnAction(e -> {
             logger.info(String.format("pressed backspace, lastInput = %d, currentInput = %d", lastInput, currentInput));
@@ -90,7 +181,7 @@ public class UICreator {
         btnEquals.setOnAction(e -> {
             logger.info(String.format("pressed equals, lastInput = %d, currentInput = %d, operator = %s, lastResult = %d ",
                     lastInput, currentInput, lastOperator, lastResult));
-            if (StringUtils.isNotEmpty(output.getText())) {
+            if (StringUtils.isNotEmpty(output.getText())  && StringUtils.isNotEmpty(output.getText()) && StringUtils.isNotEmpty(lastOperator)) {
                 clearLabel = true;
                 if (output.getText().length() > 19){
                     currentInput = 0;
@@ -102,53 +193,6 @@ public class UICreator {
                 }
             }
         });
-
-        vBox.getChildren().setAll(btnClear, btnBack, btnPlus, btnMinus, btnTimes, btnDivide, btnEquals);
-        return vBox;
-    }
-
-    private static long calculate(long lInput, long cInput, String lOperator) {
-        logger.info(String.format("calling calculate, lastInput = %d, currentInput = %d, lastoperator = %s ",
-                lInput, cInput, lOperator));
-        long result = 0;
-        switch (lOperator){
-            case "+":
-                result = lInput + cInput;
-                break;
-            case "-":
-                result = lInput - cInput;
-                break;
-            case "X":
-                result = lInput * cInput;
-                break;
-            case "/":
-                result = lInput / cInput;
-                break;
-            default:
-                break;
-        }
-        return result;
-
-    }
-
-    public static GridPane displayNumber(){
-
-//        VBox vBox = new VBox(10);
-        // 添加数字键
-        GridPane numberSection = new GridPane();
-        numberSection.setPadding(new Insets(5,5,5,5));
-        numberSection.setVgap(10);
-        numberSection.setHgap(10);
-        Button btnNum0 = new Button("0");
-        Button btnNum1 = new Button("1");
-        Button btnNum2 = new Button("2");
-        Button btnNum3 = new Button("3");
-        Button btnNum4 = new Button("4");
-        Button btnNum5 = new Button("5");
-        Button btnNum6 = new Button("6");
-        Button btnNum7 = new Button("7");
-        Button btnNum8 = new Button("8");
-        Button btnNum9 = new Button("9");
 
         // setting activity
         btnNum0.setOnAction(e -> {
@@ -232,29 +276,37 @@ public class UICreator {
             clearLabel = false;
         });
 
-        GridPane.setConstraints(btnNum1, 0,0);
-        GridPane.setConstraints(btnNum2, 1,0);
-        GridPane.setConstraints(btnNum3, 2,0);
-        GridPane.setConstraints(btnNum4, 0,1);
-        GridPane.setConstraints(btnNum5, 1,1);
-        GridPane.setConstraints(btnNum6, 2,1);
-        GridPane.setConstraints(btnNum7, 0,2);
-        GridPane.setConstraints(btnNum8, 1,2);
-        GridPane.setConstraints(btnNum9, 2,2);
-        GridPane.setConstraints(btnNum0, 1,3);
-        numberSection.getChildren().setAll(btnNum0,btnNum1,btnNum2,btnNum3,btnNum4,btnNum5,btnNum6,btnNum7,btnNum8,btnNum9);
-
-//        GridPane.setHalignment(output, HPos.RIGHT);
         output.setText("0");
-//        vBox.getChildren().setAll(output, numberSection);
-//        Scene scene = new Scene(numberSection);
-//        window.setScene(scene);
-//        window.show();
-        return numberSection;
+
+        return centerSection;
     }
 
     private static boolean clearCurrentLabel(){
         return clearLabel || (output.getText().length() == 1 && StringUtils.equalsIgnoreCases(output.getText(),"0"));
+    }
+
+    private static long calculate(long lInput, long cInput, String lOperator) {
+        logger.info(String.format("calling calculate, lastInput = %d, currentInput = %d, lastoperator = %s ",
+                lInput, cInput, lOperator));
+        long result = 0;
+        switch (lOperator){
+            case "+":
+                result = lInput + cInput;
+                break;
+            case "-":
+                result = lInput - cInput;
+                break;
+            case "X":
+                result = lInput * cInput;
+                break;
+            case "/":
+                result = lInput / cInput;
+                break;
+            default:
+                break;
+        }
+        return result;
+
     }
 
 }
